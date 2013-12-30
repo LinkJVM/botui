caab3a92bf217df35e6bc0ebbcd27ca68b53e8e8
kleiinnn <m@mklein.co.at>
2013-12-30 11:35:21 +0100

    added extension to binary file

--- src/KissCompileProvider.cpp
+++ src/KissCompileProvider.cpp
@@ -59,7 +59,7 @@ Compiler::OutputList KissCompileProvider::compile(const QString &name, const Kis
 		success &= out.isSuccess();
 	}
 	
-	QString result = QDir(binariesPath()).filePath(name);
+	QString result = QDir(binariesPath()).filePath(name) + "." + QFileInfo(terminalFiles[0]).completeSuffix();
 	QFile::remove(result);
 	
 	if(!success) return ret;
